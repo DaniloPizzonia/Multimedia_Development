911b4633c6fafa6d86d3f41e27f3d75677d67235
Danilo Pizzonia <danilo.pizzonia@gmx.net>
2018-10-27 19:55:11 +0200

    added save class to serialize the status

---
+++ Test_WpfApplication1/MasterConverter/Classes/Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterConverter.Classes {
+    class Converter {
+        public Converter() {
+            this.name = "hi";
+            this._Modulo = 10;
+        }
+
+        public string name { get; set; }
+        public int _Modulo { get; set; }
+
+        public int myMethod () {
+            var testInput = 41;
+
+            var mod = testInput % this._Modulo;
+
+            return mod;
+        }
+    }
+}
---
+++ Test_WpfApplication1/MasterConverter/Classes/Save.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Windows;
+
+namespace MasterConverter.Classes {
+    public class Save {
+        public static void saveObject<T>(T obj, string dataFileName) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs;
+            try {
+                fs = new FileStream(dataFileName, FileMode.Create);
+                bf.Serialize(fs, obj);
+                fs.Close();
+            } catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static T readDataFile<T>(string dataName) {
+            T data;
+            FileStream fs;
+            BinaryFormatter bf;
+            try {
+                fs = new FileStream(dataName, FileMode.Open);
+                bf = new BinaryFormatter();
+                data = (T)bf.Deserialize(fs);
+                return data;
+            } catch(Exception) {
+                return default(T);
+                throw;
+            }
+        }
+
+        public static void saveXML<T>(T data, string fileName) {
+            XmlSerializer oXmlSerializer = new XmlSerializer(typeof(T));
+            FileStream oStream;
+            oStream = new FileStream(fileName, FileMode.Create);
+            oXmlSerializer.Serialize(oStream, data);
+            oStream.Close();
+        }
+
+        public static T readXML<T>(string fileName) {
+            try {
+                using(StreamReader oStreamreader = new StreamReader(fileName)) {
+                    XmlSerializer oXmlSerializer = new XmlSerializer(typeof(T));
+                    return (T)oXmlSerializer.Deserialize(oStreamreader);
+                }
+            } catch(Exception) {
+
+                return default(T);
+            }
+        }
+    }
+}
